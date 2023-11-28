cd3c9ca4f1e351869d8d42198ce3f3009668cffe
LongTran <tnkhanhlong0201@gmail.com>
2023-11-28 22:26:33 +0700

    cở mít

--- BM.Models/LoginModel.cs
+++ BM.Models/LoginModel.cs
@@ -1,8 +1,12 @@
-﻿namespace BM.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BM.Models;
 
 public class LoginRequestModel
 {
+    [Required(ErrorMessage = "Vui lòng điền Tên đăng nhập")]
     public string UserName { get; set; }
+    [Required(ErrorMessage = "Vui lòng điền mật khẩu")]
     public string? Password { get; set; }
     public string? BranchId { get; set; }
 }
