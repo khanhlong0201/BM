5d9bd7528d39e8dec0f9de1e9866dc144433646e
LongTran <tnkhanhlong0201@gmail.com>
2023-11-30 23:54:10 +0700

    done api supplies

---
+++ BM.Models/SuppliesModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BM.Models
+{
+    public class SuppliesModel
+    {
+        [Required(ErrorMessage = "Vui lòng điền Mã vật tư")]
+        public string? SuppliesCode { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng điền Tên vật tư")]
+        public string? SuppliesName { get; set; }
+
+
+        [Required(ErrorMessage = "Vui lòng điền Đơn vị tính")]
+        public string? EnumId { get; set; }
+        public string? EnumName { get; set; }
+        public DateTime? DateCreate { get; set; }
+        public int? UserCreate { get; set; }
+        public DateTime? DateUpdate { get; set; }
+        public int? UserUpdate { get; set; }
+    }
+}
