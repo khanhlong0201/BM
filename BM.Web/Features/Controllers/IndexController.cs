6f59aa85dc62f1c7e393856fcc7240f1e481afc3
Hai Nguyen <nguyenhai15040206@gmail.com>
2023-12-13 14:28:46 +0700

    commit

--- BM.Web/Features/Controllers/IndexController.cs
+++ BM.Web/Features/Controllers/IndexController.cs
@@ -7,6 +7,8 @@ using BM.Web.Shared;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using Telerik.Blazor;
 using Telerik.Blazor.Components;
 
 namespace BM.Web.Features.Controllers
@@ -18,7 +20,14 @@ namespace BM.Web.Features.Controllers
         [Inject] private ICliMasterDataService? _masterDataService { get; init; }
         [Inject] NavigationManager? _navigationManager { get; set; }
         #endregion
-       
+
+        #region Properties
+        public DateTime StartDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public SchedulerView CurrView { get; set; } = SchedulerView.Month;
+        public IEnumerable<SheduleModel> ListSchedulers = new List<SheduleModel>();
+        #endregion
+
 
         #region Override Functions
         protected override async Task OnInitializedAsync()
@@ -44,8 +53,39 @@ namespace BM.Web.Features.Controllers
             {
                 try
                 {
+                    StartDate = _dateTimeService!.GetCurrentVietnamTime();
+                    StartTime = new DateTime(StartDate.Year, StartDate.Month, StartDate.Day, 23,0,0);
+                    ListSchedulers = new List<SheduleModel>()
+                    {
+                        new SheduleModel()
+                        {
+                            Id = 1,
+                            Start = DateTime.Now,
+                            End = DateTime.Now,
+                            Title = "Trả nợ Trả nợ chi Trang ơi",
+                            Description = "Trả nợ chi Trang ơi",
+                            IsAllDay = true,
+                        },
+                        new SheduleModel()
+                        {
+                            Id = 2,
+                            Start = DateTime.Now,
+                            End = DateTime.Now,
+                            Title = "Trả nợ 2",
+                            Description = "Trả nợ chi Trang ơi 2",
+                            IsAllDay = true,
+                        },
+                        new SheduleModel()
+                        {
+                            Id = 2,
+                            Start = DateTime.Now,
+                            End = DateTime.Now,
+                            Title = "Trả nợ 2",
+                            Description = "Trả nợ chi Trang ơi 2",
+                            IsAllDay = true,
+                        }
+                    };
                     await _progressService!.SetPercent(0.4);
-
                 }
                 catch (Exception ex)
                 {
@@ -62,5 +102,20 @@ namespace BM.Web.Features.Controllers
         #endregion
 
     
+    }
+
+    public class SheduleModel
+    {
+        public int Id { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public bool IsAllDay { get; set; }
     }
 }
